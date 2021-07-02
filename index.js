bba412935b20e2a8a03ea2c8401873a440245151
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-02 10:39:41 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+//notes 
+const dogsName = 'Sir Woody BarksALot';
+const [title, firstName, lastName] = dogsName.split(" "); // breaks the sting into pieces @ " " 
+console.log(title, firstName, lastName); // LOG: Sir Woody BarksALot
+console.log(title, lastName) //since we broke the string into elements of an aray we can call to specific parts of the array.
+
+const doggie = {
+  name: 'Buzz',
+  breed: 'Great Pyrenees',
+  furColor: 'black and white',
+  activityLevel: 'sloth-like',
+  favoriteFood: 'hot dogs'
+};
+
+const { namee, breed } = doggie;
+namee; // => "Buzz"
+breed; // => "Great Pyrenees"
+
+const doggie2 = {
+  name: 'Buzz',
+  breed: 'Great Pyrenees',
+  furColor: 'black and white',
+  activityLevel: 'sloth-like',
+  favoriteFoods: {
+    meats:{
+      ham: 'smoked',
+      hotDog: 'Oscar Meyer',
+    },
+    cheeses:{
+      american: 'kraft'
+    }
+  }
+};
+
+const { ham, hotDog } = doggie2.favoriteFoods.meats;
+ham; // => "smoked"
+hotDog; // => "Oscar Meyer"
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+
+// assignment
+
 const farmAnimals = 'cow horse sheep pig chicken';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
@@ -28,21 +70,24 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ")
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ")
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+const [blackAndWhite, ,black, pink,] = farmAnimals.split(" ")
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
+const [r, o, y, g, b, , v] = colors
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [, , , , , indg, ] = colors
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
-
+const {muppetName, color, song, job, partner} = muppet
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {song2, song4} = nestedMuppet.album.theMuppetMovie
+const {nestedJob, nestedPartner} = nestedMuppet
